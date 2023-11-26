d00f191ee36fb08383950bc83f031b5164575c0c
Emma <mishaelsema@gmail.com>
2023-11-26 22:36:41 +0100

    none

--- Week_2/Day_1/Daily_Challenge/daily_challenge.py
+++ Week_2/Day_1/Daily_Challenge/daily_challenge.py
@@ -6,8 +6,8 @@ elif len(string)>10:
 elif len(string)==10:
     print('perfect string')
     print(string[0],string[-1])
+for i in range(len(string)):
+    print(string[:i+1])
+    
 
-for i in range(0, len(string)+1):
-    strings=string[i],string[i+1]
-print(strings)
 
